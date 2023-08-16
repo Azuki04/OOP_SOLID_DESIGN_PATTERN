51c72c3a0c1902cb0931e5699aaec07b557b83e2
Sy Viet <sy.dao@cec.valantic.com>
2023-08-16 11:28:08 +0200

    initialise new object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,36 @@
+import demo.shape.AllAreaCalculator;
 import demo.shape.Circle;
+import demo.shape.Rectangle;
+import demo.shape.Shape;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Circle circle = new Circle(0, 0, 3);
 
+        // AllAreaCalculator
+        AllAreaCalculator areaCalculator = new AllAreaCalculator();
+
+        // circle1
+        Circle circle = new Circle(0, 0, 3);
+        // square1
+        Rectangle rectangle = new Rectangle(0, 0, 3, 3);
+
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(circle);
+        shapes.add(rectangle);
+
+        double sum = areaCalculator.sum(shapes);
+
+        System.out.println(sum);
 
         double result = circle.calculateArea();
 
-        System.out.printf("Area of circle with diameter %d is %f\n", circle.getDiameter(), result);
+        System.out.print(result);
 
     }
 
